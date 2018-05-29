e232cc133c47d75ed05da5e4788f3e707e96459c
b0ndman <iraluqbond@gmail.com>
2018-05-28 16:44:57 -0800

    fixing voiceless erros

--- spellcheck.py
+++ spellcheck.py
@@ -11,25 +11,26 @@ import re
 
 def spellcheck(word):  #returns true when all spell checks have passed, otherwise returns false
     word = word.lower()  #sterilize capitilizations
+    result = True
     if re.search(r"[^-'acegiklmnpqrstuvwy]", word):  #illegal letters or special characters not use
         print("English letter or illegal symbol error")
-        return False
-    elif re.search(r"[aui]{3,}|[auie]e|e[eaui]", word): #triple vowels or vowels with e
+        result = False
+    if re.search(r"[aui]{3,}|[auie]e|e[eaui]", word): #triple vowels or vowels with e
         print("Triple vowels or improper vowel error")
-        return False
-    elif re.search(r"([glrsv])\1\1|([ckmnpqtwy])\2", word): #two or more non-fricatives or 3 or more fricatives
+        result = False
+    if re.search(r"([glrsv])\1\1|([ckmnpqtwy])\2", word): #two or more non-fricatives or 3 or more fricatives
         print("consonant combination error")
-        return False
-    elif re.search(r"[ckpqt]'*([glrsv])\1|([glrsv])\2'*[ckpqt]", word): #stop followed/preceded by two or more fricatves
+        result = False
+    if re.search(r"[ckpqt]'*([glrsv])\1|([glrsv])\2'*[ckpqt]", word): #stop followed/preceded by two or more fricatves
         print("stop devoicing error")
-        return False
-    elif re.search(r"([glrsv])\1([glrsv])\2", word):
+        result = False
+    if re.search(r"([glrsv])\1([glrsv])\2|[glrsv]([glrsv])\3", word): #voiceless fricative followed by two fricatives
         print("fricative devoicing error")
-        return False
-    elif re.search(r"[cgklmnpqrstvwy]{3,}", word):
+        result = False
+    if re.search(r"[cgklmnpqrstvwXy]{3,}", re.sub(r"([glrsv])\1|ng", "X", word)):
         print("triple consonant error")
-        return False
-    else:
-        return True
+        result = False
+    #print(re.sub(r"([glrsv])\1", "X", word))
+    return result
 
-#print(spellcheck("ayagg'tuq"))
+#print(spellcheck("Beayakkaggtarllinirtluni"))
